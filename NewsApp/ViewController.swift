927dc1744e6b4e2ed2b5a4fee047a790840b2f7d
Deirdre Storck <deirdre.storck@gmail.com>
2016-08-09 15:13:43 -0700

    initial setup of app - includes a few buttons on home screen that are connected to IBactions

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -12,7 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        assignBackground()
+    }
+    
+    func assignBackground(){
+        let background = UIImage(named: "background")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIViewContentMode.ScaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +33,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func getWorldNews(sender: AnyObject) {
+        print("let's get the world news!")
+    }
 
+    @IBAction func getUSANews(sender: AnyObject) {
+        print("usa news")
+    }
+    
+    @IBAction func getSeattleNews(sender: AnyObject) {
+        print("get some seattle news")
+    }
+    
+    @IBAction func getPoliticalNews(sender: AnyObject) {
+        print("hear how depressing politics are")
+    }
+    
+    @IBAction func getScienceNews(sender: AnyObject) {
+        print("science.boom!")
+    }
+    
+    
+    @IBAction func getCultureNews(sender: AnyObject) {
+        print("culture thyself")
+    }
+    
+    
 }
 
