72435cdee420d8f030c78ca0b567cca4696ccc1b
Deirdre Storck <deirdre.storck@gmail.com>
2016-08-10 15:40:26 -0700

    got app to hit my rails api and get a response but the response is a string

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -35,6 +35,45 @@ class ViewController: UIViewController {
 
     @IBAction func getWorldNews(sender: AnyObject) {
         print("let's get the world news!")
+        let url = "http://localhost:3000/section/world"
+        let myUrl = NSURL(string: url)
+        let request = NSMutableURLRequest(URL:myUrl!)
+        request.HTTPMethod = "GET"
+        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
+            data, response, error in
+            
+            if error != nil
+            {
+                print("error=\(error)")
+                return
+            }
+            
+            // Print out response string
+            let responseString = NSString(data: data!, encoding: NSUTF8StringEncoding)
+            print("responseString = \(responseString)")
+
+            
+            
+            // Convert server json response to NSDictionary
+            do {
+                if let convertedJsonIntoDict = try NSJSONSerialization.JSONObjectWithData(data!, options: []) as? NSDictionary {
+                    
+                    // Print out dictionary
+                    print(convertedJsonIntoDict)
+                    
+                    // Get value by key
+                    let firstNameValue = convertedJsonIntoDict["userName"] as? String
+                    print(firstNameValue!)
+                    
+                }
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+            
+        }
+        
+        task.resume()
+        
     }
 
     @IBAction func getUSANews(sender: AnyObject) {
