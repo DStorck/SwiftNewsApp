c7ec869eb0fae06524df4d83c2810d59065e1c6f
Deirdre Storck <deirdre.storck@gmail.com>
2016-08-11 15:04:30 -0700

    using swiftyJson to get data and as of now pushing data from seattle API call into articles array

--- NewsApp/ViewController.swift
+++ NewsApp/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 import Alamofire
-
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
@@ -49,34 +49,57 @@ class ViewController: UIViewController {
         }
         
     }
-
+    
     @IBAction func getUSANews(sender: AnyObject) {
         print("usa news")
-        Alamofire.request(.GET, "http://localhost:3000/us-news")
-            .responseJSON { response in
-                
-                if let JSON = response.result.value {
-                   // print("JSON: \(JSON)")
-                    let arr = JSON as! NSArray
-                    for item in arr {
-                        print(item["title"])
-                    }
+        Alamofire.request(.GET, "http://localhost:3000/us-news").validate().responseJSON { response in
+            switch response.result {
+            case .Success:
+                var articles = [String]()
+                if let value = response.result.value {
+                    let json = JSON(value)
+                    let firstTitle = json[0]["title"].stringValue
+                    articles.append(firstTitle)
+                    //print(json[0]["title"].stringValue)
+                    print("first title")
+                    print(firstTitle)
+                    print(articles)
                 }
+            case .Failure(let error):
+                print(error)
+            }
+            
+            
         }
     }
-    
+    var articles = [String]()
     @IBAction func getSeattleNews(sender: AnyObject) {
         print("get some seattle news")
-        Alamofire.request(.GET, "http://localhost:3000/seattle")
-            .responseJSON { response in
-                
-                if let JSON = response.result.value {
-                    let arr = JSON as! NSArray
-                    for item in arr {
-                        print(item["title"])
+        Alamofire.request(.GET, "http://localhost:3000/us-news").validate().responseJSON { response in
+            switch response.result {
+            case .Success:
+                if let value = response.result.value {
+                    let json = JSON(value)
+                    for (_,subJson):(String, JSON) in json {
+                        let tit = String(subJson["title"])
+                        //print(subJson["title"])
+                        self.articles.append(tit)
+                        
                     }
+                    print(self.articles)
+    
+                  
+                    //print(json[0]["title"].stringValue)
+                   
                 }
+            case .Failure(let error):
+                print(error)
+            }
+            
+            
         }
+       
+
     }
     
     @IBAction func getPoliticalNews(sender: AnyObject) {
@@ -122,8 +145,10 @@ class ViewController: UIViewController {
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        print("this is printing right away isn't it?")
+        print(articles)
         let DestViewController: ArticlesViewController = segue.destinationViewController as! ArticlesViewController
-        DestViewController.article = "you are a turkey face"
+        DestViewController.article = String(self.articles.count)
     }
     
     
